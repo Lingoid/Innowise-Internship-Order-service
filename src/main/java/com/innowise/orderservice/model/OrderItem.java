9bf967877cefa8e1c1b880fb90f1f16ed2a0fa98
Максим Багель <maksik2000000@gmail.com>
2025-10-11 11:56:44 +0300

    feat(db): added Liquibase schema and Entities for Order, Item, and OrderItem

---
+++ src/main/java/com/innowise/orderservice/model/OrderItem.java
@@ -0,0 +1,31 @@
+package com.innowise.orderservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "order_items")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderItem {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id", nullable = false)
+    private Item item;
+
+    @Column(nullable = false)
+    private Integer quantity;
+}
